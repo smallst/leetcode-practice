6869ee5629df5959a8e0a141ed10ba7ec3ef4473
smallst <tongsmallst@gmail.com>
2019-03-01 20:55:26 +0800

    331. Verify Preorder Serialization of a Binary Tree

--- main.cpp
+++ main.cpp
@@ -1,50 +1,27 @@
 class Solution {
 public:
-    int kth(vector<int>& nums1, vector<int>& nums2, int as, int ae, int bs, int be, int k)
+    bool isValidSerialization(string preorder) {
+        int count = 0;
+        int l = preorder.length();
+        if(preorder[l-1] != '#')
+            return false;
+        for(int i = 0; i< l-1; i++)
         {
-            int al = ae - as;
-            int bl = be - bs;
-            if(al <= 0)
+            if(preorder[i] == '#')
             {
-                return nums2[bs + k - 1];
+                count --;
+                if(i<l-1){i++;}
             }
-            if(bl <= 0)
-            {
-                return nums1[as + k - 1];
-            }
-            if(k == 1)
-            {
-                return nums1[as] < nums2[bs] ? nums1[as] : nums2[bs];
-            }
-            int asplit = k * al / (al + bl);
-            if(asplit == 0) asplit = 1;
-            int bsplit = k - asplit;
-            if(nums1[as + asplit-1] <= nums2[bs + bsplit-1])
-            {
-                return kth(nums1, nums2, as + asplit, ae, bs, bs + bsplit, k-asplit);
-            }
-            else
-            {
-                return kth(nums1, nums2, as, as + asplit, bs + bsplit, be, k-bsplit );
-            }
+                else
+                {
+                    count ++;
+                    while(i<l-1&&preorder[++i] !=',');
+                }
+            if(count < 0 )
+                return false;
         }
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int la = nums1.size();
-        int lb = nums2.size();
-        if((la + lb)% 2 == 0)
-        {
-            int k = (la + lb)/2;
-            // cout<<kth(nums1, nums2, 0, la, 0, lb, k)<<endl;
-            // cout<<kth(nums1, nums2, 0, la, 0, lb, k+1)<<endl<<endl;;
-            return (kth(nums1, nums2, 0, la, 0, lb, k) + kth(nums1, nums2, 0, la, 0, lb, k+1))/2.0;
-        }
-        else
-        {
-            int k = (la + lb) / 2 + 1;
-            // cout<<kth(nums1, nums2, 0, la, 0, lb, k)<<endl<<endl;;;
-            return kth(nums1, nums2, 0, la, 0, lb, k);
-        }
+        if(count == 0)
+            return true;
+        return false;
     }
-
 };
-;
