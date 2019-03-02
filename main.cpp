1af70131c22ae1c0c0347eb591e0938bc6774987
smallst <tongsmallst@gmail.com>
2019-03-02 18:51:41 +0800

    284. Peeking Iterator

--- main.cpp
+++ main.cpp
@@ -1,50 +1,46 @@
-class Solution {
+// Below is the interface for Iterator, which is already defined for you.
+// **DO NOT** modify the interface for Iterator.
+class Iterator {
+    struct Data;
+	Data* data;
 public:
-    bool checkValid(int a,int b,int X,int Y)
-    {
-        if(a < 0 || a >= X  || b < 0 || b >=Y)
-            return true;
-        return false;
-    }
-    void gameOfLife(vector<vector<int>>& board) {
-        int X = board.size();
-        int Y = (*board.begin()).size();
-        for(auto x = 0; x != X ; x++)
-        {
-            for(auto y = 0; y != Y; y++)
-            {
-                int count = 0;
-                for(int i = -1; i< 2; i++)
-                {
-                    for(int j = -1; j< 2; j++)
-                    {
-                        if((i ==0 && j == 0) || checkValid(x+i, y+j, X, Y))
-                        {
-                            continue;
-                        }
-                        count += (board[x+i][y+j]&1);  
-                    }
-                }
-                if(count == 3)
-                {
-                    board[x][y] |= (1<<1);
-                }
-                else if(count < 2 || count > 3)
-                {
-                    board[x][y] &= 1;
-                }
-                else
-                {
-                    board[x][y] |= (board[x][y] << 1);
-                }
-            }
-        }
-        for(auto x = 0; x != X ; x++)
-        {
-            for(auto y = 0; y != Y; y++)
-            {
-                board[x][y] >>= 1;
-            }
-        }
-    }
+	Iterator(const vector<int>& nums);
+	Iterator(const Iterator& iter);
+	virtual ~Iterator();
+	// Returns the next element in the iteration.
+	int next();
+	// Returns true if the iteration has more elements.
+	bool hasNext() const;
+};
+
+
+class PeekingIterator : public Iterator {
+public:
+    bool isHasNext;
+    int nextValue;
+	PeekingIterator(const vector<int>& nums) : Iterator(nums) {
+	    // Initialize any member here.
+	    // **DO NOT** save a copy of nums and manipulate it directly.
+	    // You should only use the Iterator interface methods.
+	    isHasNext = Iterator::hasNext();
+        if(isHasNext) nextValue = Iterator::next();
+	}
+
+    // Returns the next element in the iteration without advancing the iterator.
+	int peek() {
+        return nextValue;
+	}
+
+	// hasNext() and next() should behave the same as in the Iterator interface.
+	// Override them if needed.
+	int next() {
+	    isHasNext = Iterator::hasNext();
+        int value = nextValue;
+        if(isHasNext) nextValue = Iterator::next();
+        return value;
+	}
+
+	bool hasNext() const {
+	    return isHasNext;
+	}
 };
