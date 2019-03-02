908f952b8e4992890a7a63eacaec8a743d4df231
smallst <tongsmallst@gmail.com>
2019-03-02 16:18:12 +0800

    309. Best Time to Buy and Sell Stock with Cooldown

--- main.cpp
+++ main.cpp
@@ -1,61 +1,22 @@
 class Solution {
 public:
-    vector<int> findMinHeightTrees(int n, vector<pair<int, int>>& edges) {
-        vector<int> res;
-        if(n <=2)
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        if(n < 2)
         {
-            for(int i = 0;i < n ; i++)
-            {
-                res.push_back(i);
-            }
-            return res;
+            return 0;
         }
-        vector<unordered_set<int>> graph(n);
-        int *node = new int [n];
-        for(int i = 0;i < n; i++)
+        int * buy = new int [n];
+        int * sell = new int [n];
+        buy[0] = -prices[0];
+        sell[0] = 0;
+        buy[1] = prices[0] > prices[1] ? -prices[1] : -prices[0];
+        sell[1] = buy[0] + prices[1] > 0 ?buy[0] + prices[1] : 0;
+        for(int i =2; i< n; i++)
         {
-            node[i] = 0;
+            buy[i] = buy[i-1] > sell[i-2] - prices[i] ? buy[i-1] : sell[i-2] -prices[i];
+            sell[i] = buy[i-1] + prices[i] > sell[i-1] ? buy[i-1] + prices[i] : sell[i-1];
         }
-        for(auto i = edges.begin(); i != edges.end(); i++)
-        {
-            node[(*i).first] ++;
-            node[(*i).second] ++;
-            graph[(*i).first].insert((*i).second);
-            graph[(*i).second].insert((*i).first);
-        }
-        queue<int> lis;
-        for(int i=0; i< n; i++)
-        {
-            if(node[i] == 1)
-            {
-                lis.push(i);
-            }
-        }
-        while(n > 2)
-        {
-            int l = lis.size();
-            n = n - l;
-            for(int ii = 0; ii < l; ii ++)
-            {
-                int i = lis.front();
-            auto it = graph[i].begin(); 
-                    node[*it] --; 
-                graph[*it].erase(i);
-                if(node[*it] == 1)
-                {
-                    lis.push(*it);
-                }
-            node[i] = 0;
-            lis.pop();
-            }
-        }
-        
-       while(!lis.empty())
-       {
-           res.push_back(lis.front());
-           lis.pop();
-       }
-            
-        return res;
+        return sell[n-1];
     }
 };
