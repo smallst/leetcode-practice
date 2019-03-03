c5b1012a6ddd7b209d807ebdee96e46de15b1277
smallst <tongsmallst@gmail.com>
2019-03-03 20:18:04 +0800

    162. Find Peak Element

--- main.cpp
+++ main.cpp
@@ -1,42 +1,34 @@
 class Solution {
 public:
-    unordered_map<long, int> dict;
-    string fractionToDecimal(int numerato, int denominato) {
-        
-        long numerator= numerato, denominator =denominato;
-        bool flag = true;
-        if(numerator < 0 && denominator >= 0)
+    int findPeakElement(vector<int>& nums) {
+        int left = 0, right = nums.size();
+        while(left + 1 < right)
         {
-            flag =false;
+            int mid = (left + right) / 2;
+            if(mid == 0 && nums[mid] > nums[mid+1])
+            {
+                return 0;
+            }
+            if(mid + 1 == nums.size())
+                if(nums[mid] > nums[mid-1])
+                return mid;
+            else{
+                right = mid;
+                continue;
+            }
+            if(nums[mid] > nums[mid-1])
+            {
+                if( nums[mid] > nums[mid+1])
+                return mid;
+                else
+                {
+                    left = mid;
+                }
+            }
+            else{
+                right = mid;
+            }
         }
-        if(numerator >= 0 && denominator < 0)
-        {
-            flag =false;
-        }
-                numerator = numerator < 0 ? -numerator:numerator;
-        denominator = denominator < 0 ? -denominator:denominator;
-
-        int index = 1;
-        long orig = numerator % denominator;
-        long iii = numerator / denominator;
-        if(!orig)
-                return iii == 0 ? to_string(iii) : ((flag?"":"-") + to_string(iii));
-
-        else
-        dict[orig] = index++;
-        long temp = orig * 10 % denominator;
-        string repeat = to_string(orig*10/denominator);
-        while(temp && dict[temp]== 0)
-        {
-            dict[temp] = index ++;
-            repeat += to_string(temp * 10 / denominator);
-            temp = temp* 10 % denominator;
-        }
-        if(temp && dict[temp] != 0)
-        {
-            return (flag?"":"-") + to_string(iii) + "." + repeat.substr(0, dict[temp]-1) + "(" + repeat.substr(dict[temp]-1, repeat.length()) + ")";
-        }
-        
-        return (flag?"":"-") + to_string(iii) + "." + repeat;
-}
+        return left;
+    }
 };
