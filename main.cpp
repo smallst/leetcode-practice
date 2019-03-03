f4000ab8fc7ef3040eae382218a1333d3ee8c6c1
smallst <tongsmallst@gmail.com>
2019-03-03 19:26:13 +0800

    166. Fraction to Recurring Decimal

--- main.cpp
+++ main.cpp
@@ -1,50 +1,42 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
-class BSTIterator {
-    private:
-    stack<TreeNode*> st;
+class Solution {
 public:
-    void findLeft(TreeNode* root)
-    {
-        TreeNode * temp = root;
-        while(temp!=NULL)
+    unordered_map<long, int> dict;
+    string fractionToDecimal(int numerato, int denominato) {
+        
+        long numerator= numerato, denominator =denominato;
+        bool flag = true;
+        if(numerator < 0 && denominator >= 0)
         {
-            st.push(temp);
-            temp = temp->left;
+            flag =false;
         }
-    }
-    BSTIterator(TreeNode* root) {
-        findLeft(root);
-    }
-    
-    /** @return the next smallest number */
-    int next() {
-        TreeNode * head = st.top();
-        int val = head->val;
-        st.pop();
-        if(head->right)
+        if(numerator >= 0 && denominator < 0)
         {
-            findLeft(head->right);
+            flag =false;
         }
-        return val;
-    }
-    
-    /** @return whether we have a next smallest number */
-    bool hasNext() {
-        return !st.empty();
-    }
-};
+                numerator = numerator < 0 ? -numerator:numerator;
+        denominator = denominator < 0 ? -denominator:denominator;
 
-/**
- * Your BSTIterator object will be instantiated and called as such:
- * BSTIterator* obj = new BSTIterator(root);
- * int param_1 = obj->next();
- * bool param_2 = obj->hasNext();
- */
+        int index = 1;
+        long orig = numerator % denominator;
+        long iii = numerator / denominator;
+        if(!orig)
+                return iii == 0 ? to_string(iii) : ((flag?"":"-") + to_string(iii));
+
+        else
+        dict[orig] = index++;
+        long temp = orig * 10 % denominator;
+        string repeat = to_string(orig*10/denominator);
+        while(temp && dict[temp]== 0)
+        {
+            dict[temp] = index ++;
+            repeat += to_string(temp * 10 / denominator);
+            temp = temp* 10 % denominator;
+        }
+        if(temp && dict[temp] != 0)
+        {
+            return (flag?"":"-") + to_string(iii) + "." + repeat.substr(0, dict[temp]-1) + "(" + repeat.substr(dict[temp]-1, repeat.length()) + ")";
+        }
+        
+        return (flag?"":"-") + to_string(iii) + "." + repeat;
+}
+};
