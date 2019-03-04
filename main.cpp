7f6b67888ae3320c523108b2e6b8ab94e0f0fc34
smallst <tongsmallst@gmail.com>
2019-03-04 14:56:06 +0800

    31. Next Permutation

--- main.cpp
+++ main.cpp
@@ -1,12 +1,45 @@
 class Solution {
 public:
-    double myPow(double x, int n) {
-        double res = 1;
-        for(int i = n; i != 0; i/=2)
+    void nextPermutation(vector<int>& nums) {
+        int l = nums.size(), i;
+        for(i = l - 1; i > 0; i--)
         {
-            if(i%2) res *= x;
-            x *= x;
+            if(nums[i] > nums[i-1])
+            {
+                break;
+            }
         }
-       return n < 0 ?1/res:res;
+        if(i==0)
+        {
+            reverse(nums, 0, l-1);
+            return;
+        }
+        i--;
+        for(int j = l-1; j>i;j--)
+        {
+            if(nums[i] < nums[j])
+            {
+                swap(nums, i, j);
+                reverse(nums, i+1, l-1);
+                return;
+            }
+        }
+    }
+    void swap(vector<int> & nums, int a, int b)
+    {
+        if(a >= b)
+            return;
+        nums[a] = nums[a] + nums[b];
+        nums[b] = nums[a] - nums[b];
+        nums[a] = nums[a] - nums[b];
+    }
+    void reverse(vector<int> & nums, int s, int e)
+    {
+        if(s >= e)
+            return;
+        for(int i =s; i <= (s+e)/2; i++)
+        {
+            swap(nums, i, s+e-i);
+        }
     }
 };
