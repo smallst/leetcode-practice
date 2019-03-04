5d4e03e4d963761b3308d4ea1e696fd62b247458
smallst <tongsmallst@gmail.com>
2019-03-04 15:21:46 +0800

    17. Letter Combinations of a Phone Number

--- main.cpp
+++ main.cpp
@@ -1,23 +1,35 @@
 class Solution {
 public:
-   
-    void generateTail(vector<string>& v, string s, int n, int m){
-    if(n == 0 && m == 0)
-    {
-        v.push_back(s);
-        return;
+    map<char, vector<char>> d2s;
+    vector<string> letterCombinations(string digits) {
+        char c, i;
+        for(c = 'a', i = '2'; c < 'y'; i++)
+        {
+            d2s[i].push_back(c++);
+            d2s[i].push_back(c++);
+            d2s[i].push_back(c++);
+            if(i == '7')
+                d2s[i].push_back(c++);
+        }
+        d2s['9'].push_back('z');
+        
+        vector<string> res;
+        if(digits.size() == 0)
+            return res;
+        mapDS(res, digits, "");
+        return res;
     }
-    if(m > 0) generateTail(v, s+")", n, m-1);
-    if(n > 0) generateTail(v, s+"(", n-1, m+1);
-}
-vector<string> generateParenthesis(int n) {
-    std::vector<string> res;
-    if(n == 0)
-    return res;
-    generateTail(res, "", n, 0);
-    return res;
-}
-
-
-
+    void mapDS(vector<string>& res, string digits, string str){
+        int l = digits.size();
+        if(l == 0)
+        {
+            res.push_back(str);
+            return;
+        }
+        vector<char> temp = d2s[digits[0]];
+        for(auto i = temp.begin();i != temp.end(); i++)
+        {
+            mapDS(res, digits.substr(1, l-1), str + (*i));
+        }
+    }
 };
